86d26babecd3922e33221c5190436028e1980192
c2yz <c2yzawa@outlook.com>
2026-02-22 15:39:16 +0800

    fix: 666 clap最新版4.5.60开头的

--- src/command.rs
+++ src/command.rs
@@ -1,13 +1,13 @@
 /*
  *
- *  * Created: 2026-2-22 3:29:8
- *  * File: main.rs
+ *  * Created: 2026-2-22 3:37:17
+ *  * File: command.rs
  *  * The whole project follows MIT LICENSE.
  *  * Copyright (c) 2026 The Open Team. All rights reserved.
  *
  */
-pub mod command;
-
-fn main() {
+#[derive(Parser)]
+struct Cli {
 
 }
+
